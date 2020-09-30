f642c68e537394fbd8d2d1232067e250e6c0e1c9
bakmenson <bakmenson@gmail.com>
2020-09-30 23:45:33 +0300

    returns to staticmethods

--- choose_ide.py
+++ choose_ide.py
@@ -3,8 +3,8 @@
 
 class ChooseIde:
 
-    @classmethod
-    def choose_ide(cls, ide_array: tuple) -> str:
+    @staticmethod
+    def choose_ide(ide_array: tuple) -> str:
         ide_range: int = len(ide_array) + 1
 
         print()
@@ -14,23 +14,23 @@ class ChooseIde:
 
         print(f"{ide_range}. Exit.")
 
-        cls.ide_name = ide_array[ChooseIde.__choose_integer_in_range(ide_range) - 1]
+        ide_number = ChooseIde.__choose_integer_in_range(ide_range)
 
-        return cls.ide_name
+        return list(ide_array)[ide_number - 1]
 
-    @classmethod
-    def __choose_integer_in_range(cls, integer_range: int) -> int:
+    @staticmethod
+    def __choose_integer_in_range(integer_range: int) -> int:
         while True:
             try:
-                cls.ide_number = int(input(">>> "))
+                number = int(input(">>> "))
             except ValueError:
                 print(f"Number most be integer between: 0 - {integer_range}")
                 continue
 
-            if 0 < cls.ide_number <= integer_range:
+            if 0 < number <= integer_range:
                 break
 
             print(f"Number most be between: 0 - {integer_range}")
             continue
 
-        return cls.ide_number
+        return number
