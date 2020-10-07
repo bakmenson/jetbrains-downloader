4e99d5a8ec23b6f55ceff8205a404a82c03dc178
bakmenson <bakmenson@gmail.com>
2020-10-07 22:55:36 +0300

    added product version for return

--- products.py
+++ products.py
@@ -6,23 +6,26 @@ from sys import exit
 
 class Products:
 
-    def __init__(self, products: Union[list, tuple]) -> None:
-        self.__products: Union[list, tuple] = products
+    def __init__(self, products: dict) -> None:
+        self.__products: Union[dict, tuple] = products
         self.__products_with_index: dict = {}
         self.__product_name: str = ""
-        self.__index: int = 0
+        self.__product_version: str = ""
+        self.__index: int = len(self.__products) + 1
 
-    def get_product_name(self) -> str:
-        return self.__product_name
+    def get_product(self) -> dict:
+        return {self.__product_name: self.__product_version}
 
     def choose_product(self) -> None:
         self.__choose_product()
+        self.__product_version = self.__products_with_index[self.__index][1]
 
-        if isinstance(self.__products_with_index[self.__index], tuple):
-            self.__products = self.__products_with_index[self.__index][1]
+        if isinstance(self.__products_with_index[self.__index][0], tuple):
+            self.__products = self.__products_with_index[self.__index][0][1]
             self.__choose_product()
-
-        self.__product_name = self.__products_with_index[self.__index]
+            self.__product_name = self.__products_with_index[self.__index]
+        else:
+            self.__product_name = self.__products_with_index[self.__index][0]
 
     def __choose_product(self) -> None:
         self.__set_index_for_products()
@@ -34,12 +37,18 @@ class Products:
 
     def __set_index_for_products(self) -> None:
         self.__products_with_index.clear()
-        for index, item in enumerate(self.__products, start=1):
-            self.__products_with_index[index] = item
+        if isinstance(self.__products, dict):
+            for index, item in enumerate(self.__products.items(), start=1):
+                self.__products_with_index[index] = item
+        else:
+            for index, item in enumerate(self.__products, start=1):
+                self.__products_with_index[index] = item
 
     def __print_products(self) -> None:
         for key, value in self.__products_with_index.items():
-            if isinstance(value, tuple):
+            if isinstance(value[0], tuple):
+                print(f"{key:>2}. {value[0][0]}")
+            elif isinstance(value, tuple):
                 print(f"{key:>2}. {value[0]}")
             else:
                 print(f"{key:>2}. {value}")
